fcb23cabf40644efb0edd1e63bb5aba774f8caeb
Anna-Chumakova <ann020187@gmail.com>
2022-07-17 11:27:04 +0300

    gallery2

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -3,9 +3,16 @@ import { galleryItems } from './gallery-items.js';
 
 console.log(galleryItems);
 const galleryRef = document.querySelector(".gallery");
+
+
 const galleryList = galleryItems.map(({ preview, original, description }) => {
     return `<a class="gallery__item" href="${original}">
   <img class="gallery__image" src="${preview}" alt="${description}" />
 </a>`;
 }).join(" ");
 galleryRef.insertAdjacentHTML("beforeend", galleryList);
+
+ const gallerySlide = new SimpleLightbox(".gallery a", {
+  captionsData: "alt",
+  captionDelay: 250,
+});
